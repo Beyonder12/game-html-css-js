a9b0e2b5265b55c6b38193bfb80aa06f48f99632
Fajri Illahi <beyonder@LAPTOP-44CPTEGU.localdomain>
2023-07-01 21:45:43 +0700

    Made the pacman bigger after eat the two dots

---
+++ pacman/READMEE.md
@@ -0,0 +1,6 @@
+just run server using python
+
+python3 -m http.server 9999
+
+then you can open browser http://localhost:9999
+
--- pacman/script.js
+++ pacman/script.js
@@ -3,9 +3,11 @@ var pacman = {
   y: 0,
   speed: 1,
   direction: 'right',
+  size: 20
 };
 
 var gameBoard = document.getElementById('game-board');
+var dots = Array.from(document.getElementsByClassName('dot')); // get all dot elements
 
 function update() {
   var pacmanElement = document.getElementById('pacman');
@@ -15,6 +17,11 @@ function update() {
     pacmanElement.id = 'pacman';
     gameBoard.appendChild(pacmanElement);
   }
+
+  pacmanElement.style.width = `${pacman.size}px`;  // Update the size of pacman
+  pacmanElement.style.height = `${pacman.size}px`; // Update the size of pacman
+  pacmanElement.style.left = `${pacman.x}px`;
+  pacmanElement.style.top = `${pacman.y}px`;
 
   let newX = pacman.x;
   let newY = pacman.y;
@@ -34,18 +41,38 @@ function update() {
       break;
   }
 
-  if(newX >= 0 && newX <= (gameBoard.offsetWidth - pacmanElement.offsetWidth)) {
+  // check if pacman would hit a dot
+  for (let i = 0; i < dots.length; i++) {
+    let dot = dots[i];
+    let dotX = dot.offsetLeft + dot.offsetWidth / 2;
+    let dotY = dot.offsetTop + dot.offsetHeight / 2;
+    let dx = newX + pacman.size / 2 - dotX;
+    let dy = newY + pacman.size / 2 - dotY;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    
+    if (distance < (pacman.size + dot.offsetWidth) / 2) {
+      dot.parentNode.removeChild(dot); // remove the dot from the game-board
+      dots.splice(i, 1); // remove the dot from the dots array
+      pacman.size += 2; // increase pacman's size
+      pacmanElement.style.width = `${pacman.size}px`;  // Update the size of pacman
+      pacmanElement.style.height = `${pacman.size}px`; // Update the size of pacman
+      break;
+    }
+  }
+
+  // check if pacman would go out of bounds
+  if(newX >= 0 && newX <= (gameBoard.offsetWidth - pacman.size) && newY >= 0 && newY <= (gameBoard.offsetHeight - pacman.size)) {
     pacman.x = newX;
-  }
-
-  if(newY >= 0 && newY <= (gameBoard.offsetHeight - pacmanElement.offsetHeight)) {
     pacman.y = newY;
+  } else {
+    alert('Game over!');
+    pacman.x = 0; // reset pacman's position
+    pacman.y = 0;
+    pacman.size = 20; // reset pacman's size
+    pacmanElement.style.width = `${pacman.size}px`;  // Reset the size of pacman
+    pacmanElement.style.height = `${pacman.size}px`; // Reset the size of pacman
   }
-
-  pacmanElement.style.left = `${pacman.x}px`;
-  pacmanElement.style.top = `${pacman.y}px`;
 }
-
 
 document.addEventListener('keydown', function(e) {
   switch (e.key) {
@@ -64,6 +91,4 @@ document.addEventListener('keydown', function(e) {
   }
 });
 
-
 setInterval(update, 1000 / 60); // Update the game 60 times per second.
-
